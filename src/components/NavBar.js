4a72c511aefb1e4240afcf912d7571a7c21aaaa0
Kidd254 <lorenkioko@gmail.com>
2023-07-17 22:40:39 +0300

    import styling into the file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import {useSelector} from 'react-redux'
+import {useSelector} from 'react-redux';
+import styles from '../Styles/Nav.module.css';
 
 const Navbar = () => {
     const { amount } = useSelector ((state) => state.cart)
     return (
-        <nav>
-        <div className="nav-center">
-            <h1>Redux Toolkit</h1>
+        <nav className={styles.nav}>
+        <div className={styles.center}>
+            <h1>Cart Items</h1>
             <AiOutlineShoppingCart />
         <div>{amount}</div>
             </div>
