4e031cbfdd4abba6a9087c3e6114f4efaf9c35a6
Kidd254 <lorenkioko@gmail.com>
2023-07-17 13:48:55 +0300

    add icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import {useSelector} from 'react-redux'
 
 const Navbar = () => {
+    const { amount } = useSelector ((state) => state.cart)
     return (
         <nav>
         <div className="nav-center">
             <h1>Redux Toolkit</h1>
+            <AiOutlineShoppingCart />
+        <div>{amount}</div>
             </div>
         </nav>
     )
