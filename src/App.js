baa5bcdfa04bcd73867f8f0a9e09f7d55f63dc71
Kidd254 <lorenkioko@gmail.com>
2023-07-17 13:51:16 +0300

    render nav bar to browser

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 
 import './App.css';
+import Navbar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <h1>
-          JavaScript State Management Library
-        </h1>
-          
-
-
-      </header>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 
