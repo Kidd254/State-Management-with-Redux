4a85ee385d70969c23956447779ad481b826cfdf
Kidd254 <lorenkioko@gmail.com>
2023-07-17 20:36:02 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Navbar from './components/NavBar'
+import Navbar from './components/NavBar';
+import Cart from './features/cart/displayItems';
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Cart />
     </main>
   );
 }
