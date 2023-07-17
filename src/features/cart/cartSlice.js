3a41a5a3d8e24f0d9784f971743677f3c81c7ab6
Kidd254 <lorenkioko@gmail.com>
2023-07-17 13:58:31 +0300

    add and remove items from cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
 
 const initialState ={
-    cartItems: [],
+    cartItems: cartItems,
     amount: 0,
     total: 0,
     isLoading: true,
@@ -10,6 +11,21 @@ const initialState ={
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-})
+    reducers: {
+    add: (state, action) => {
+      const newObject = {
+        title: action.payload.title,
+        author: action.payload.author,
+      };
+      state.bookList.push(newObject);
+    },
+    remove: (state, action) => {
+      state.bookList.filter((books) => (
+        books.id !== action.payload.id
+      ));
+    },
+  },
+});
 
+export const { add, remove } = cartSlice.actions;
 export default cartSlice.reducer
