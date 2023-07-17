4a61adb3426bb39924947def62951803d6e5577f
Kidd254 <lorenkioko@gmail.com>
2023-07-17 13:44:59 +0300

    add nav bar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import {useSelector} from 'react-redux'
+
+const Navbar = () => {
+    return (
+        <nav>
+        <div className="nav-center">
+            <h1>Redux Toolkit</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
