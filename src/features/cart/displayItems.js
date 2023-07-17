c45f952486e39adcefa1a0cc60074058a1316f4a
Kidd254 <lorenkioko@gmail.com>
2023-07-17 22:41:06 +0300

    add styling

--- src/features/cart/displayItems.js
+++ src/features/cart/displayItems.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import {add, remove} from './cartSlice';
+import styles from '../../Styles/Display.module.css';
 
 function Cart() {
   // Access the cartItems from the Redux store or state
@@ -15,15 +16,27 @@ const dispatch = useDispatch();
   };
 
   return (
-    <div>
+    <div className={styles.display}>
+  
+      <div className={styles.heading}>
       <h2>Cart Items:</h2>
+      </div>
       {cartItems.map(item => (
-        <><div key={item.id}>
-              <p>Title: {item.title}</p>
-              <p>Price: {item.price}</p>
-              <img src={item.img} alt={item.title} />
+        <div className= {styles.itemsContainer} key={item.id}>
+          <div className={styles.columnone}>
+              <p className={styles.title}>Title: {item.title}</p>
+              <p className={styles.price}>Price: {item.price}</p>
+              <img className={styles.image} src={item.img} alt={item.title} />
+              <div className={styles.btnContainer}>
+              <button className={styles.btn} onClick={handleAddItem}>Add</button><button className={styles.btn2}onClick={handleRemoveItem}>Remove</button>
+              </div>
+              </div>
+             
+              <div className={styles.column}>
               <p>Amount: {item.amount}</p>
-          </div><button onClick={handleAddItem}>Add</button><button onClick={handleRemoveItem}>Remove</button></>
+              </div>
+               </div>
+          
       ))}
     </div>
   );
